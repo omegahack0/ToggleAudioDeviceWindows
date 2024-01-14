fa07ab83549edbae0a93de677f9c5cbc96166855
omegahack0 <32969902+omegahack0@users.noreply.github.com>
2024-01-14 18:34:05 -0500

    Add files via upload

---
+++ AudioSwitcher.ps1
@@ -0,0 +1,9 @@
+﻿  ##This requires the cmdlet from https://github.com/frgnca/AudioDeviceCmdlets
+  ##Run Install-Module -Name AudioDeviceCmdlets as an admin
+  ##Start by running "get-audiodevice -list" and indentifying the 2 audio sources you want
+  ##Write down their Index Numbers. 
+  $V = Get-audiodevice -playback
+  if ($V.Index -eq "4") ##Change the "A" here to one of the index numbers
+  {set-audiodevice -index 1} ##Change the "B" here to the other index number
+  else 
+  {set-audiodevice -index 4} ##Change the "A" here to the index number you used on line 5. Save and test. That's it!
